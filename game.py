0661e9a201c1e3e404c07836fab7d24343eba3ec
Trealer <127015607+Emmanuelkhisa@users.noreply.github.com>
2025-05-20 13:22:58 +0300

    Update game.py

--- game.py
+++ game.py
@@ -1,4 +1,4 @@
-from random import random, choice
+from random import choice
 
 print('''
  ---------------------------------------------------
@@ -9,36 +9,42 @@ print('''
 ''')
 
 Choices = ['ROCK', 'PAPER', 'SCISSOR']
-print(" ROCK,  PAPER OR  SCISSOR  ?")
-print("\n")
-user = input("YOUR CHOICE: ")
-Computer = choice(Choices)
 
-if user.upper() =="ROCK":
-    if Computer =="PAPER":
-        print("I FOLD YOU !! LOOOOOOOSSEEERRR")
-    elif Computer == "SCISSOR":
-        print("DANG !! YOU WIN")
+while True:
+    print("\nROCK, PAPER OR SCISSOR?")
+    user = input("YOUR CHOICE: ").upper()
+    Computer = choice(Choices)
+
+    if user == "ROCK":
+        if Computer == "PAPER":
+            print("I FOLD YOU !! LOOOOOOOSSEEERRR")
+        elif Computer == "SCISSOR":
+            print("DANG !! YOU WIN")
+        else:
+            print("IT'S A DRAW")
+
+    elif user == "PAPER":
+        if Computer == "ROCK":
+            print("EASY ON ME !!, YOU WIN")
+        elif Computer == "SCISSOR":
+            print("NICE TO CUT YOU, LOOOOOOSSEEERR")
+        else:
+            print("I THINK IT'S A DRAW")
+
+    elif user == "SCISSOR":
+        if Computer == "ROCK":
+            print("HARD ON YOU, LOOOOOOSSEEERR !")
+        elif Computer == "PAPER":
+            print("CUTS, * YOU WIN *")
+        else:
+            print("DRAW, LET'S ROLL AGAIN")
+
     else:
-        print("IT'S A DRAW")
+        print(f"INVALID >> *{user}* CHECK YOUR CHOICE AGAIN")
 
-elif user.upper() == "PAPER":
-    if Computer == "ROCK":
-        print("EASY ON ME !!, YOU WIN")
-    elif Computer == "SCISSOR":
-        print("NICE TO CUT YOU, LOOOOOOSSEEERR")
-    else:
-        print("I THINK IT'S A DRAW")
+    print(f'COMPUTER CHOICE IS: {Computer}')
 
-elif user.upper() == "SCISSOR":
-    if Computer == "ROCK":
-        print("HARD ON YOU, LOOOOOOSSEEERR !")
-    elif Computer == "PAPER":
-        print("CUTS, * YOU WIN *")
-    else:
-        print("DRAW, LET'S ROLL AGAIN")
-
-else:
-    print(f"INVALID >> *{user}* CHECK YOUR CHOICE AGAIN")
-
-print(f'COMPUTER CHOICE IS: {Computer}')
+    play_again = input("\nDo you want to play again? (yes/no): ").strip().lower()
+    if play_again != 'yes':
+        print("\nThanks for playing. Goodbye!")
+        break
